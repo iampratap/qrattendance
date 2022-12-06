da381ec92b90eb1c1ee89a1e4704ae29b5d14ed9
varsha surve <varshasurve13@gmail.com>
2022-12-06 14:21:00 +0530

    get All batches done

--- src/app/pages/batch/batch.component.ts
+++ src/app/pages/batch/batch.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component} from '@angular/core';
+import { RestService } from 'src/app/services/rest.service';
 
 @Component({
   selector: 'app-batch',
   templateUrl: './batch.component.html',
   styleUrls: ['./batch.component.css']
 })
-export class BatchComponent {
 
+export class BatchComponent {
+  batches : any[] = [];
+  
+  constructor(private _rest : RestService){
+
+  }
+  ngOnInit(): void
+    {
+    this._rest.getAllBatches().subscribe((data : any) =>
+    {
+      console.log(data);
+      this.batches = data.data;
+    });
+  }
+
+  
+  
 }
